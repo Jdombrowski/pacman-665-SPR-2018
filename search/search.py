238eb687799c71637ef5ddca3c5d3f94351fc25e
jondo <jdombrowski416@gmail.com>
2018-02-09 12:34:42 -0800

    added node class

--- search/search.py
+++ search/search.py
@@ -71,6 +71,37 @@ def tinyMazeSearch(problem):
     w = Directions.WEST
     return [s, s, w, s, w, w, s, w]
 
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost = 0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        if parent:
+            self.path_cost = parent.path_cost + path_cost
+            self.depth = parent.depth
+        else:
+            self.path_cost = path_cost
+            self.depth = 0
+
+    def __repr__(self):
+        return "<Node %s>" % self.state
+
+    def nodePath(self):
+        x, result = self, [self]
+        while x.parent:
+            result.append(x.parent)
+            x = x.parent
+        result.reverse()
+        return result
+
+    def expand(self, problem):
+        return [Node(nextNode, self, act, cost)
+                for (nextNode, act, cost) in problem.getSuccessors(self.state)]
+
+
+
+
+
 
 def graphSearch(problem, fringe):
     """
